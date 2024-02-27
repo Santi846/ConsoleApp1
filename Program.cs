cd8b8a434cda58e53ecc32a51191ee7a81232d38
Santiago Arocha <arochas846@gmail.com>
2024-02-26 23:54:02 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+var names = new[] { "Santi", "Felipe", "Emillia" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello, {name.ToUpper()}!");
+}
