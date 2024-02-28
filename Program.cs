6397d4af0e4d50f4526d4c18d74b8ca4c56cfb76
Santiago Arocha <arochas846@gmail.com>
2024-02-27 23:57:22 -0300

    Variables

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//iterate a list
+//implicit variable decalaration
 var names = new[] { "Santi", "Felipe", "Emillia" };
+
 foreach (var name in names)
 {
     Console.WriteLine($"Hello, {name.ToUpper()}!");
 }
+
+//use a declared variable
+//explicit variable decalaration
+string nombre = "Santiago Arocha";
+Console.WriteLine(nombre);
