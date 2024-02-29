b744b329723f8c12e14fe94f84c3818dcb206187
Santiago Arocha <arochas846@gmail.com>
2024-02-29 00:25:01 -0300

    Data types variable declaration

--- Program.cs
+++ Program.cs
@@ -9,5 +9,14 @@ foreach (var name in names)
 
 //use a declared variable
 //explicit variable decalaration
+//reference decalaration
 string nombre = "Santiago Arocha";
 Console.WriteLine(nombre);
+
+//value decalaration
+int edad = 21;
+float firstNum = -20.5f;
+double secondNum = -70.5;
+decimal thirdNum = 485.623m;
+
+Console.WriteLine("Mi edad es: " + edad + "y mi primer numero es el: " + firstNum + "mi segundo numero es el:" + secondNum + " canto el numero:" + thirdNum);
