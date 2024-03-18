777632196f5cd942204ab2c2585c2877f57a2d51
Santiago Arocha <arochas846@gmail.com>
2024-03-18 00:24:45 -0300

    Intriducing classes

---
+++ Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //internal class Book
+    //{
+    //}
+
+    public class Book
+    {
+        string _name;
+        string _author;
+        string _description;
+        int _amount_pages;
+
+        //contructor that instances each attribute
+        public Book( string name, string author, string description, int amount_pages)
+        {
+            _name = name;
+            _author = author;
+            _description = description;
+            _amount_pages = amount_pages;
+        }
+
+        //methor for returning that constructor built
+        public string GetDescription()
+        {
+            return $"Book {_name} by {_author}";
+        }
+    }
+}
